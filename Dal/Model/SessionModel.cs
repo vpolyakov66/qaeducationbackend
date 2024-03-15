9adaac40c79c27f95e335dce8206db45ba04d2ff
vpoliakov <vpoliakov@nopaper.ru>
2024-03-15 11:22:47 +0500

    +

--- Dal/Model/SessionModel.cs
+++ Dal/Model/SessionModel.cs
@@ -6,6 +6,7 @@ namespace qaeducationbackend.Dal.Model
         public int Id { get; set; }
         public DateTime SessionDate { get; set; }
         public string ClientName { get; set; }
+        public int AgeClient { get; set; }    
         public FilmModel Film { get; set; }
 
         public static implicit operator List<object>(SessionModel v)
